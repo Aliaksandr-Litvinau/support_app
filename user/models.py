6723e879d4621ea8bd3632c9390d8f6aa0d6a85c
litvinau_alex <litvinaualex@gmail.com>
2022-07-07 10:07:15 +0200

    create model user

---
+++ user/models.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    """
+    Кастомная модель пользователя
+    """
+
+    username = models.CharField(max_length=64, unique=True,
+                                verbose_name='Имя пользователя')
+    email = models.EmailField(unique=True, blank=False, null=False,
+                              verbose_name='Email')
+    is_support = models.BooleanField(default=False,
+                                     verbose_name='Сотрудник саппорта')
